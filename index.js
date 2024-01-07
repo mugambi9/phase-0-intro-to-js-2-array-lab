7812134a613918e13eb6632be9a97c34b17bb843
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-07 22:26:58 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+
+let cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function appendCat(name) {
+    // Return a new array with the cat appended
+    return [...cats, name];
+  }
+  
+  function prependCat(name) {
+    
+    return [name, ...cats];
+  }
+  
+  function removeLastCat() {
+    // Return a new array with the last cat removed
+    return cats.slice(0, -1);
+  }
+  
+  function removeFirstCat() {
+    // Return a new array with the first cat removed
+    return cats.slice(1);
+  }
+  
+  
+
+
+
+
+
